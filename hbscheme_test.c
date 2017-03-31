46d004ea44e4a42a4167f409e330a691d4dd496f
Hannes Buchwald <hb052@hdm-stuttgart.de>
2017-03-31 14:57:25 +0200

    Wrong test case

--- hbscheme_test.c
+++ hbscheme_test.c
@@ -33,7 +33,7 @@ void unitTest() {
 	/***** a2l Test *****/
 
 	// positive test
-	ASSERT( a2l("2") == 2, "a2l error");
+	ASSERT( a2l("0") == 2, "a2l error");
 	ASSERT( a2l("1") == 1, "a2l error");
 	ASSERT( a2l("123456") == 123456, "a2l error");
 	ASSERT( a2l("999999") == 999999, "a2l error");

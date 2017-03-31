d1168fb5a7abd9d0e168578725adf86d551546f5
Hannes Buchwald <hb052@hdm-stuttgart.de>
2017-03-31 15:04:35 +0200

    3 = 3

--- hbscheme_test.c
+++ hbscheme_test.c
@@ -33,11 +33,9 @@ void unitTest() {
 	/***** a2l Test *****/
 
 	// positive test
-<<<<<<< HEAD
-	ASSERT( a2l("1") == 2, "a2l error");
-=======
+
+	ASSERT( a2l("3") == 3, "a2l error");
 	ASSERT( a2l("0") == 2, "a2l error");
->>>>>>> master
 	ASSERT( a2l("1") == 1, "a2l error");
 	ASSERT( a2l("123456") == 123456, "a2l error");
 	ASSERT( a2l("999999") == 999999, "a2l error");
